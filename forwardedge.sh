f395f37e956587570ce4aa0a1b17987b3ee520d2
Marty Leisner <maleisner@gmail.com>
2026-01-23 15:18:55 -0500

    script to help builds

---
+++ forwardedge.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+
+# defaults to putting the output in $PWD/output
+# figure out if we're crossing with the contents of $CC
+
+set -e
+
+OUTPUT_DIRECTORY=$PWD/output
+
+echo putting output in $OUTPUT_DIRECTORY
+
+
+if [  ! -e configure ]; then
+	echo running autogen
+	./autogen.sh
+fi
+
+exit
+
+function test_crosscompiler
+{
+	if [ -z "${CC}" ]; then
+		return;
+	fi
+
+	OLDIFS="$IFS"
+	IFS=-
+	
+	set -- ${CC}
+
+	echo Number of options $#
+	if [ $# -gt 3  ]
+	then
+		HOST_CONFIG="--host=$1-$2-$3"
+		export CROSS=1
+
+	fi
+	IFS="$OLDIFS"
+}
+
+
+
+
+test_crosscompiler 
+
+cp wolfcrypt/src/fips_test.c.bad wolfcrypt/src/fips_test.c
+
+# to cross compile add --host=aarch64-linux-gnu and $CROSS=1
+#
+./configure  --enable-mlkem --enable-mldsa -prefix=$OUTPUT_DIRECTORY  "$HOST_CONFIG"
+make
+./forwardedge-fips.hash.sh
+make
+./run_testwolfcrypt.sh
+if [ $? -ne 0 ]
+then
+	echo testwolfcrypt failed
+	exit 1
+fi
+
+make install
+exit 0
+
